85d5032723bd9f0b1ee9b48b598d3f4a14324c44
Cocozzj <zhan7565@umn.edu>
2023-03-12 22:16:35 -0500

    Update

--- violinPlot.py
+++ violinPlot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from utils.context import *
 
 if __name__ == '__main__':
-    fullDataset = pd.read_csv(path.join(DATA_DIR, 'full-dataset.csv'))
+    fullDataset = pd.read_csv(path.join(DATA_DIR, 'full-dataset.csv'),low_memory=False)
 
     # =====================================================================================
     abstractTechLabel = 'Abstract-Tech-Label'
@@ -20,22 +20,22 @@ if __name__ == '__main__':
     # =====================================================================================
     data = uploadDataSet.groupby(abstractTechLabel)[tputUpLabel].agg(lambda x: list(x))
     labels = data.index
-    fig, ax = plt.figure(figsize=(10, 7))
+    fig, ax = plt.subplots()
 
     ax.violinplot(data, showmeans=True, showmedians=True)
     ax.set_ylabel('Speed (Mbps)',fontsize=25)
     ax.set_xlabel('Deployment Type',fontsize=25)
-    ax.set_xticks(np.arange(len(labels) + 1), labels=labels,fontsize=25)
+    ax.set_xticks(np.arange(1, len(labels) + 1), labels=labels,fontsize=25)
     plt.title('Upload Speeds',fontsize=25)
     plt.grid(linestyle='--', axis='y')
     plt.tight_layout()
-    plt.savefig(path.join(PLOT_DIR, 'violin-UL-general.png'))
+    plt.savefig(path.join(TASK1_DIR, 'violin-UL-general.png'))
     plt.close()
 
     # # =====================================================================================
     data = downloadDataSet.groupby(abstractTechLabel)[tputDlLabel].agg(lambda x: list(x))
     labels = data.index
-    fig, ax = plt.subplots(figsize=(10, 7))
+    fig, ax = plt.subplots()
 
     ax.violinplot(data, showmeans=True, showmedians=True)
     ax.set_ylabel('Speed (Mbps)',fontsize=25)
@@ -45,6 +45,5 @@ if __name__ == '__main__':
     plt.title('Download Speeds',fontsize=25)
     plt.tight_layout()
 
-    plt.savefig(path.join(PLOT_DIR, 'violin-DL-general.png'))
+    plt.savefig(path.join(TASK1_DIR, 'violin-DL-general.png'))
     plt.close()
-    # =====================================================================================
